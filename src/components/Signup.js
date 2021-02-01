12a9da2327f3790a45278ec7dd9ed7d566cb486a
Sevi <correosevi@gmail.com>
2021-02-01 14:12:03 +0000

    Add Form and Form components

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,6 +7,23 @@ export default function Signup() {
       <Card>
         <Card.body>
           <h2 className="text-center mb-4">Sign Up</h2>
+          <Form>
+            <Form.Group id="email">
+              <Form.label>Email</Form.label>
+              <Form.control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.label>Password</Form.label>
+              <Form.control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.label>Password Confirmation</Form.label>
+              <Form.control type="password" ref={PasswordConfirmRef} required />
+            </Form.Group>
+            <Button className="w-100" type="submit">
+              Sign Up
+            </Button>
+          </Form>
         </Card.body>
       </Card>
       <div className="w-100 text-center mt-2">
