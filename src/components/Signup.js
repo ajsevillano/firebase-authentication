5372fbbf55e5fd1cb2b494411f1193c272bc8fb5
Sevi <correosevi@gmail.com>
2021-02-01 19:53:15 +0000

    create and export useAuth

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,13 @@
 import React, { useRef } from 'react';
 import { Card, Form, Button } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function Signup() {
   //Create the Ref
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup } = useAuth;
 
   return (
     <>
