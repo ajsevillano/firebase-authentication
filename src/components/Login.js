95a3dd6267673cd75a995cf9ffa3ce6bfbbb9721
Sevi <correosevi@gmail.com>
2022-01-22 19:48:38 +0000

    Add a check to redirect login screen if user is logged

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, Redirect, Route } from 'react-router-dom';
 
 export default function Login() {
   const emailRef = useRef();
@@ -10,6 +10,7 @@ export default function Login() {
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
   const history = useHistory();
+  const { currentUser } = useAuth();
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -22,37 +23,42 @@ export default function Login() {
     } catch {
       setError('Failed to Log in');
     }
-
-    setLoading(false);
   }
 
   return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Log in</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Log in
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/forgot-password">Forgot password?</Link>
+    // If we are logged, we redirect to the dashboard
+    <Route>
+      {currentUser ? (
+        <Redirect to="/" />
+      ) : (
+        <>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Log in</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" ref={passwordRef} required />
+                </Form.Group>
+                <Button disabled={loading} className="w-100" type="submit">
+                  Log in
+                </Button>
+              </Form>
+              <div className="w-100 text-center mt-3">
+                <Link to="/forgot-password">Forgot password?</Link>
+              </div>
+            </Card.Body>
+          </Card>
+          <div className="w-100 text-center mt-2">
+            Need an account? <Link to="/Signup"> Sign Up</Link>
           </div>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/Signup"> Sign Up</Link>
-      </div>
-    </>
+        </>
+      )}
+    </Route>
   );
 }
